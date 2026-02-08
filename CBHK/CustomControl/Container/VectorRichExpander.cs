e38f0e0cb674ca86f2b27697de433014f6d6e46e
洪荒太初_cber <2682265891@qq.com>
2026-02-08 12:16:37 +0800

    已大概实现自定义标头的Expander

---
+++ CBHK/CustomControl/Container/VectorRichExpander.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CBHK.CustomControl.Container
+{
+    public class VectorRichExpander : Expander
+    {
+        public object HeaderContent
+        {
+            get { return GetValue(HeaderContentProperty); }
+            set { SetValue(HeaderContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderContentProperty =
+            DependencyProperty.Register("HeaderContent", typeof(object), typeof(VectorRichExpander), new PropertyMetadata(default(object)));
+
+        public DataTemplate HeaderContentTemplate
+        {
+            get { return (DataTemplate)GetValue(HeaderContentTemplateProperty); }
+            set { SetValue(HeaderContentTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderContentTemplateProperty =
+            DependencyProperty.Register("HeaderContentTemplate", typeof(DataTemplate), typeof(VectorRichExpander), new PropertyMetadata(default(DataTemplate)));
+    }
+}
